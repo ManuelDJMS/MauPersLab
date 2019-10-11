60b6e939b74996771cb2c4d2b3e2dc98e2430186
Manuel Morales <46793411+ManuelDJMS@users.noreply.github.com>
2019-10-11 17:43:22 -0500

    Primera Version

---
+++ Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmHome
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GunaPanel1 = New Guna.UI.WinForms.GunaPanel()
+        Me.GunaPanel2 = New Guna.UI.WinForms.GunaPanel()
+        Me.GunaPanel3 = New Guna.UI.WinForms.GunaPanel()
+        Me.GunaVSeparator1 = New Guna.UI.WinForms.GunaVSeparator()
+        Me.GunaPictureBox1 = New Guna.UI.WinForms.GunaPictureBox()
+        Me.GunaImageButton3 = New Guna.UI.WinForms.GunaImageButton()
+        Me.GunaImageButton2 = New Guna.UI.WinForms.GunaImageButton()
+        Me.GunaImageButton1 = New Guna.UI.WinForms.GunaImageButton()
+        Me.GunaLabel1 = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaVSeparator2 = New Guna.UI.WinForms.GunaVSeparator()
+        Me.GunaPanel1.SuspendLayout()
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GunaPanel1
+        '
+        Me.GunaPanel1.Controls.Add(Me.GunaVSeparator2)
+        Me.GunaPanel1.Controls.Add(Me.GunaLabel1)
+        Me.GunaPanel1.Controls.Add(Me.GunaPictureBox1)
+        Me.GunaPanel1.Controls.Add(Me.GunaVSeparator1)
+        Me.GunaPanel1.Controls.Add(Me.GunaImageButton3)
+        Me.GunaPanel1.Controls.Add(Me.GunaImageButton2)
+        Me.GunaPanel1.Controls.Add(Me.GunaImageButton1)
+        Me.GunaPanel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GunaPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.GunaPanel1.Name = "GunaPanel1"
+        Me.GunaPanel1.Size = New System.Drawing.Size(800, 50)
+        Me.GunaPanel1.TabIndex = 0
+        '
+        'GunaPanel2
+        '
+        Me.GunaPanel2.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.GunaPanel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GunaPanel2.Location = New System.Drawing.Point(0, 50)
+        Me.GunaPanel2.Name = "GunaPanel2"
+        Me.GunaPanel2.Size = New System.Drawing.Size(800, 50)
+        Me.GunaPanel2.TabIndex = 1
+        '
+        'GunaPanel3
+        '
+        Me.GunaPanel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GunaPanel3.Location = New System.Drawing.Point(0, 100)
+        Me.GunaPanel3.Name = "GunaPanel3"
+        Me.GunaPanel3.Size = New System.Drawing.Size(800, 500)
+        Me.GunaPanel3.TabIndex = 2
+        '
+        'GunaVSeparator1
+        '
+        Me.GunaVSeparator1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GunaVSeparator1.LineColor = System.Drawing.Color.WhiteSmoke
+        Me.GunaVSeparator1.Location = New System.Drawing.Point(707, 12)
+        Me.GunaVSeparator1.Name = "GunaVSeparator1"
+        Me.GunaVSeparator1.Size = New System.Drawing.Size(10, 25)
+        Me.GunaVSeparator1.TabIndex = 3
+        '
+        'GunaPictureBox1
+        '
+        Me.GunaPictureBox1.BaseColor = System.Drawing.Color.White
+        Me.GunaPictureBox1.Image = Global.Maulabs.My.Resources.Resources.icons8_verified_account_28
+        Me.GunaPictureBox1.Location = New System.Drawing.Point(587, 8)
+        Me.GunaPictureBox1.Name = "GunaPictureBox1"
+        Me.GunaPictureBox1.Size = New System.Drawing.Size(32, 32)
+        Me.GunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.GunaPictureBox1.TabIndex = 4
+        Me.GunaPictureBox1.TabStop = False
+        '
+        'GunaImageButton3
+        '
+        Me.GunaImageButton3.Image = Global.Maulabs.My.Resources.Resources.icons8_multiply_48
+        Me.GunaImageButton3.ImageSize = New System.Drawing.Size(16, 16)
+        Me.GunaImageButton3.Location = New System.Drawing.Point(756, 8)
+        Me.GunaImageButton3.Name = "GunaImageButton3"
+        Me.GunaImageButton3.OnHoverImage = Nothing
+        Me.GunaImageButton3.OnHoverImageOffset = New System.Drawing.Point(0, -1)
+        Me.GunaImageButton3.Size = New System.Drawing.Size(32, 32)
+        Me.GunaImageButton3.TabIndex = 2
+        '
+        'GunaImageButton2
+        '
+        Me.GunaImageButton2.Image = Global.Maulabs.My.Resources.Resources.icons8_horizontal_line_48
+        Me.GunaImageButton2.ImageSize = New System.Drawing.Size(16, 16)
+        Me.GunaImageButton2.Location = New System.Drawing.Point(718, 8)
+        Me.GunaImageButton2.Name = "GunaImageButton2"
+        Me.GunaImageButton2.OnHoverImage = Nothing
+        Me.GunaImageButton2.OnHoverImageOffset = New System.Drawing.Point(0, -1)
+        Me.GunaImageButton2.Size = New System.Drawing.Size(32, 32)
+        Me.GunaImageButton2.TabIndex = 1
+        '
+        'GunaImageButton1
+        '
+        Me.GunaImageButton1.Image = Global.Maulabs.My.Resources.Resources.icons8_menu_48
+        Me.GunaImageButton1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.GunaImageButton1.Location = New System.Drawing.Point(8, 8)
+        Me.GunaImageButton1.Name = "GunaImageButton1"
+        Me.GunaImageButton1.OnHoverImage = Nothing
+        Me.GunaImageButton1.OnHoverImageOffset = New System.Drawing.Point(0, -1)
+        Me.GunaImageButton1.Size = New System.Drawing.Size(32, 32)
+        Me.GunaImageButton1.TabIndex = 0
+        '
+        'GunaLabel1
+        '
+        Me.GunaLabel1.AutoSize = True
+        Me.GunaLabel1.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GunaLabel1.Location = New System.Drawing.Point(628, 15)
+        Me.GunaLabel1.Name = "GunaLabel1"
+        Me.GunaLabel1.Size = New System.Drawing.Size(65, 20)
+        Me.GunaLabel1.TabIndex = 5
+        Me.GunaLabel1.Text = "Manuel"
+        '
+        'GunaVSeparator2
+        '
+        Me.GunaVSeparator2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GunaVSeparator2.LineColor = System.Drawing.Color.WhiteSmoke
+        Me.GunaVSeparator2.Location = New System.Drawing.Point(561, 12)
+        Me.GunaVSeparator2.Name = "GunaVSeparator2"
+        Me.GunaVSeparator2.Size = New System.Drawing.Size(10, 25)
+        Me.GunaVSeparator2.TabIndex = 6
+        '
+        'FrmHome
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 600)
+        Me.Controls.Add(Me.GunaPanel3)
+        Me.Controls.Add(Me.GunaPanel2)
+        Me.Controls.Add(Me.GunaPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "FrmHome"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.GunaPanel1.ResumeLayout(False)
+        Me.GunaPanel1.PerformLayout()
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GunaPanel1 As Guna.UI.WinForms.GunaPanel
+    Friend WithEvents GunaImageButton3 As Guna.UI.WinForms.GunaImageButton
+    Friend WithEvents GunaImageButton2 As Guna.UI.WinForms.GunaImageButton
+    Friend WithEvents GunaImageButton1 As Guna.UI.WinForms.GunaImageButton
+    Friend WithEvents GunaPanel2 As Guna.UI.WinForms.GunaPanel
+    Friend WithEvents GunaPanel3 As Guna.UI.WinForms.GunaPanel
+    Friend WithEvents GunaPictureBox1 As Guna.UI.WinForms.GunaPictureBox
+    Friend WithEvents GunaVSeparator1 As Guna.UI.WinForms.GunaVSeparator
+    Friend WithEvents GunaVSeparator2 As Guna.UI.WinForms.GunaVSeparator
+    Friend WithEvents GunaLabel1 As Guna.UI.WinForms.GunaLabel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class FrmHome
+    Private Sub FrmHome_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Guna.UI.Lib.GraphicsHelper.ShadowForm(Me)
+        Guna.UI.Lib.GraphicsHelper.DrawLineShadow(GunaPanel3, Color.Black, 20, 10, Guna.UI.WinForms.VerHorAlign.HoriziontalTop)
+
+    End Sub
+End Class

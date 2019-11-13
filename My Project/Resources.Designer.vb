5b1bf23efdfb5e5759a83c4581ea441be6d795be
Manuel Morales <46793411+ManuelDJMS@users.noreply.github.com>
2019-11-13 12:42:00 -0600

    login

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -259,5 +259,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property LabQuimicaSanguinea() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("LabQuimicaSanguinea", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
